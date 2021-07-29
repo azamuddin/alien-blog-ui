03543d5b32a059adc0d79292d5c8a0b670fd6fe1
Muhammad Azamuddin <mas.azamuddin@gmail.com>
2021-07-29 21:26:51 +0700

    feat: link to home on ToPNav

--- components/TopNav/index.tsx
+++ components/TopNav/index.tsx
@@ -1,10 +1,13 @@
 import * as React from 'react'; 
+import Link from 'next/link';
 
 export default function TopNav(){
   return <div className="bg-gray-100 p-3">
     <div className="max-w-4xl mx-auto flex">
       <div>
-        Aliens .blog
+        <Link href="/">
+          Aliens .blog
+        </Link>
       </div>
       <div className="flex ml-auto space-x-3">
         <div className="p-1 px-3 bg-indigo-600 text-white rounded shadow cursor-pointer">
