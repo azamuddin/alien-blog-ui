715df349bcd090c90b0a70ef6a12ff99350daeff
Muhammad Azamuddin <mas.azamuddin@gmail.com>
2021-07-30 06:35:29 +0700

    feat: user info is handled globally on _app.js

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,18 +1,31 @@
 import 'tailwindcss/tailwind.css'
-import type { AppProps } from 'next/app'
+import type { AppContext, AppProps } from 'next/app'
 import { Provider } from 'react-redux'
-import {initializeStore, useStore} from '../store'
-import { withCookie } from 'next-cookie';
+import { useStore} from '../store'
+import {useCookie} from 'next-cookie'
+import {User} from '../types'
 
-function MyApp({ Component, pageProps, cookie }: AppProps) {
+function MyApp({ Component, pageProps, user }: AppProps & {user: User | null}) {
 
   let store = useStore({
-    ...pageProps.initialReduxState, 
-    user: cookie.get('alien_blog_user')
+    ...pageProps?.initialReduxState, 
+    user
   })
 
   return <Provider store={store}>
      <Component {...pageProps} />
   </Provider>
 }
-export default withCookie(MyApp)
+
+MyApp.getInitialProps = function (appContext: AppContext){
+
+  const cookie = useCookie(appContext.ctx);
+  const user = cookie.get('alien_blog_user');
+
+  return {
+    user: user || null
+  }
+
+}
+
+export default MyApp
