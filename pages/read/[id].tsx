e64c6c3cefc6f01553a31aa751be1ddfde7797a9
Muhammad Azamuddin <mas.azamuddin@gmail.com>
2021-07-30 05:45:07 +0700

    style: axios import style

--- pages/read/[id].tsx
+++ pages/read/[id].tsx
@@ -1,6 +1,7 @@
 import { NextPageContext } from 'next'; 
 import * as React from 'react'; 
 import { useRouter } from 'next/router'
+import axios from 'axios';
 import { format } from 'date-fns'; 
 
 import TopNav from '../../components/TopNav'
