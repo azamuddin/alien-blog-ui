e77056341caa3cb0a236e76ca7e1d3edd60682df
Muhammad Azamuddin <mas.azamuddin@gmail.com>
2021-07-29 21:27:14 +0700

    feat: TopNav on read page

--- pages/read/[id].tsx
+++ pages/read/[id].tsx
@@ -4,27 +4,32 @@ import { useRouter } from 'next/router'
 import * as axios from 'axios';
 import { format } from 'date-fns'; 
 
+import TopNav from '../../components/TopNav'
+
 export default function Read(props){
 
   let { query: { id } } = useRouter();
   const { post } = props; 
 
-  return <div className="max-w-4xl mx-auto text-center">
-    <div className="text-4xl font-bold mx-auto mt-10 ">
-      {post.title} 
-    </div>
-    <div className="flex space-x-2 justify-center mt-3">
-      <div>
-        {post.user?.name} 
+  return <>
+    <TopNav/>
+    <div className="max-w-4xl mx-auto text-center">
+      <div className="text-4xl font-bold mx-auto mt-10 ">
+        {post.title} 
       </div>
-      <div>
-        {format(new Date(post.createdAt), 'd/MM/Y')}
+      <div className="flex space-x-2 justify-center mt-3">
+        <div>
+          {post.user?.name} 
+        </div>
+        <div>
+          {format(new Date(post.createdAt), 'd/MM/Y')}
+        </div>
+      </div>
+      <div className="mt-10">
+        {post.content}
       </div>
     </div>
-    <div className="mt-10">
-      {post.content}
-    </div>
-  </div>
+  </>
 }
 
 
