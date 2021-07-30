aa0c8c3de7eebd24cb4721070d56cda66e806f0e
Muhammad Azamuddin <mas.azamuddin@gmail.com>
2021-07-30 08:34:28 +0700

    feat: render HTML on pages/read

--- pages/read/[id].tsx
+++ pages/read/[id].tsx
@@ -1,6 +1,5 @@
 import { NextPageContext } from 'next'; 
 import * as React from 'react'; 
-import { useRouter } from 'next/router'
 import axios from 'axios';
 import { format } from 'date-fns'; 
 
@@ -9,7 +8,6 @@ import {Post} from '../../types';
 
 export default function Read(props: { post: Post }){
 
-  let { query: { id } } = useRouter();
   const { post } = props; 
 
   return <>
@@ -27,7 +25,7 @@ export default function Read(props: { post: Post }){
         </div>
       </div>
       <div className="mt-10">
-        {post.content}
+        <div dangerouslySetInnerHTML={{__html: post.content}}></div>
       </div>
     </div>
   </>
